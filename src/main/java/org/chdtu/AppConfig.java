6d83e52cf52a419ad2b9f02ed955cc770ed66c17
Daniil Vladimirov <daniil.vladymyrov@eosda.com>
2023-11-15 21:31:56 +0200

    fix app config

--- src/main/java/org/chdtu/AppConfig.java
+++ src/main/java/org/chdtu/AppConfig.java
@@ -1,36 +1,37 @@
 package org.chdtu;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Bean
+    @Bean(name = "adminSettings")
     public UserSettings settingsAdminUser() {
         UserSettings userSettings = new UserSettings();
         userSettings.setAvatarUrl("https://example.com/img.png");
         return userSettings;
     }
 
-    @Bean
-    public User adminUser() {
+    @Bean(name = "adminUser")
+    public User adminUser(@Qualifier("adminSettings") UserSettings settings) {
         User user = new User();
         user.setUsername("admin");
-        user.setSettings(settingsAdminUser());
+        user.setSettings(settings);
         return user;
     }
 
-    @Bean
-    public DonationConfig firstDonationConfig() {
+    @Bean(name = "firstDonationConfig")
+    public DonationConfig firstDonationConfig(@Qualifier("adminUser") User user) {
         DonationConfig donationConfig = new DonationConfig();
         donationConfig.setName("базовий конфіг");
         donationConfig.setSumFrom(100F);
         donationConfig.setTextColor("#FFF");
-        donationConfig.setUser(adminUser());
+        donationConfig.setUser(user);
         return donationConfig;
     }
 
-    @Bean
+    @Bean(name = "monoMethod")
     public PaymentMethod monoPayment() {
         PaymentMethod paymentMethod = new PaymentMethod();
         paymentMethod.setName("монобанка");
@@ -39,13 +40,16 @@ public class AppConfig {
     }
 
     @Bean
-    public Donation firstDonation() {
+    public Donation firstDonation(
+            @Qualifier("firstDonationConfig") DonationConfig config,
+            @Qualifier("monoMethod") PaymentMethod paymentMethod
+    ) {
         Donation donation = new Donation();
         donation.setUsername("Kozak1337");
         donation.setText("дякую за твій контент.");
         donation.setSum(101.5F);
-        donation.setConfig(firstDonationConfig());
-        donation.setPaymentMethod(monoPayment());
+        donation.setConfig(config);
+        donation.setPaymentMethod(paymentMethod);
         return donation;
     }
 }
